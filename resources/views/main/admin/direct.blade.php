d6862f8db1aee445c0f7c42b9b8fc3732addf47c
foniks47 <novianto.tp@krakatauposco.co.id>
2023-11-24 10:51:01 +0700

    24112023

--- resources/views/main/admin/direct.blade.php
+++ resources/views/main/admin/direct.blade.php
@@ -144,6 +144,11 @@
                 "order": [
                     [0, 'desc']
                 ],
+                columnDefs: [{
+                    target: 0,
+                    visible: false,
+                    searchable: false
+                }]
                 "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
         });
